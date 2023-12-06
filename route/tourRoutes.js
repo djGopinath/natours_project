b04435132611e8c4569c188488498d87c779ae60
djGopinath <gopinath.dj619@gmail.com>
2023-12-06 16:06:07 +0530

    Added Aggregate function like matching anf grouping.

--- route/tourRoutes.js
+++ route/tourRoutes.js
@@ -10,6 +10,7 @@ const router = express.Router();
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTour);
+router.route('/tour-stats').get(tourController.getTourStats);
 router
   .route('/')
   .get(tourController.getAllTour)
