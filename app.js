0ba3dc83b845d464a870b6e28fc4feaa64289ba4
djGopinath <gopinath.dj619@gmail.com>
2024-03-02 21:37:27 +0530

    Implemented Global error handling.

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-//Own Middlware
-app.use((req, res, next) => {
-  next();
-});
+// //Own Middlware
+// app.use((req, res, next) => {
+//   next();
+// });
 //Own Middlware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -27,5 +27,27 @@ app.use((req, res, next) => {
 //Router
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'failed',
+  //   message: `Can't find ${req.originalUrl} on this sever!`
+  // });
+  const err = new Error(`Can't find ${req.originalUrl} on this sever`);
+  err.status = 'failed';
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+
+  });
+});
 
 module.exports = app;
