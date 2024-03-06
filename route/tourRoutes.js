721894ffcbec77e8b6e7fbb7c9c7e8a0e532b9f0
djGopinath <gopinath.dj619@gmail.com>
2024-03-06 11:09:50 +0530

    sign up model and routes implemented.

--- route/tourRoutes.js
+++ route/tourRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 // eslint-disable-next-line import/no-useless-path-segments
-const tourController = require('./../controllers/tourController');
+const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
 
-//Aliasing
+// Aliasing
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTour);
--- route/userRoutes.js
+++ route/userRoutes.js
@@ -1,10 +1,19 @@
 const express = require('express');
-const userController=require('./../controllers/userController')
-
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
-//User Routes
-router.route('/').get(userController.getAllUsers).post(userController.createUser);
-router.route('/:id').get(userController.getUserByID).patch(userController.updateUser).delete(userController.deleteUser);
+// User Routes
+router.post('/signup', authController.signup);
+
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+router
+  .route('/:id')
+  .get(userController.getUserByID)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
