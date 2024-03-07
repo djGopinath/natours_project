91571a83006a0cdb827abc0a3a40c025334d3a4e
djGopinath <gopinath.dj619@gmail.com>
2024-03-07 06:32:26 +0530

    Added signin and signup functionality and jwt played.

--- model/userModel.js
+++ model/userModel.js
@@ -1,5 +1,6 @@
 const mangoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 // creating a new user schema
 
@@ -20,13 +21,32 @@ const userSchema = new mangoose.Schema({
     type: String,
     required: [true, 'please provide a password'],
     minlength: 8,
+    select: false
   },
   passwordConfirm: {
     type: String,
     required: [true, 'please provide a confirm password'],
-    minlength: 8,
+    validate: {
+      validator: function (password) {
+        return password === this.password;
+      },
+      message: 'Passwords do not match',
+    },
   },
 });
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword,
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mangoose.model('User', userSchema);
 module.exports = User;
